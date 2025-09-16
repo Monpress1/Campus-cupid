3a7825ba8af92fafea9d4c388132e35cc14a25be
Monpress <elimonpresss@gmail.com>
2025-09-16 14:31:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = 'campus-cupid-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css', // If you move your CSS to a separate file
+  '/script.js', // If you move your JS to a separate file
+  '/manifest.json',
+  '/images/icons/icon-72x72.png',
+  '/images/icons/icon-96x96.png',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-144x144.png',
+  '/images/icons/icon-152x152.png',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-384x384.png',
+  '/images/icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Pacifico&family=Bebas+Neue&display=swap',
+  // Note: We don't cache the Supabase CDN links as they are not needed for offline functionality
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log('Failed to cache assets:', err);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
